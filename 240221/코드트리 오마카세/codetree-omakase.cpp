923c7cdbe8bd8a31aeed67ec9a4c5170dae358a9
Kim YangSeon <79838932+KimYangSeon@users.noreply.github.com>
2024-02-21 22:13:30 +0900

    Title: 코드트리 오마카세, Time: 226ms, Memory: 15MB, Status: Passed - CodeTree

--- 240221/코드트리 오마카세/codetree-omakase.cpp
+++ 240221/코드트리 오마카세/codetree-omakase.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 struct Customer
@@ -10,92 +10,67 @@ struct Customer
     long long x;
     string name;
     long long n;
+    long long exitTime;
 };
 
 struct Sushi
 {
     string name;
-    bool eaten;
     long long x;
     long long t;
 };
 
-map<string, Customer> customer; // 이름, 손님 정보
+map<string, Customer> customer;   // 이름, 손님 정보
 map<string, vector<Sushi>> sushi; // 이름, 이름의 초밥 벡터
-
+vector<pair<int, int>> ans;
 long long l, q;
-long long customerCnt;
-long long sushiCnt;
 
-void Eat(long long t)
+void Eat()
 {
     for (auto c = customer.begin(); c != customer.end(); c++)
     {
-        if (c->second.n == 0)
-            continue;                           // 이미 떠난 손님
-        //cout << t << ' ' << sushi[c->first].size() << ' ';
-        for(Sushi &s : sushi[c->first]){ // 손님과 이름이 같은 초밥들만 탐색
-            if(s.eaten) continue;
-            //cout << "time: " <<  s.t << ' ';
-            long long stPos, customerPos, waitingTime, ansTime;
-            customerPos = c->second.x;       // 손님 위치
+        for (Sushi s : sushi[c->second.name])
+        { // 손님과 이름이 같은 초밥들만 탐색
 
-            if (c->second.t < s.t)
-            { // 손님보다 초밥이 나중에 온 경우
-                stPos = s.x;
-                waitingTime = t - s.t;        // 손님이 기다린 시간
-                ansTime = customerPos - stPos; // 초밥이 손님 앞까지 걸리는 시간
-            }
-            else
+            long long stPos, customerPos, ansTime = 0;
+            customerPos = c->second.x; // 손님 위치
+
+            if (c->second.t < s.t) // 손님보다 초밥이 나중에 온 경우
             {
-                stPos = (c->second.t - s.t + s.x) % l; // 손님 도착 시 초밥 위치
-                waitingTime = t - c->second.t;        // 손님이 기다린 시간
-                ansTime = customerPos - stPos; // 초밥이 손님 앞까지 걸리는 시간
+                ansTime = (customerPos - s.x + l) % l; // 손님이 기다려야 하는 시간
+                ansTime += s.t;
+            }
+            else // 손님보다 초밥이 먼저 온 경우
+            {
+                stPos = (c->second.t - s.t + s.x) % l;   // 손님 도착 시 초밥 위치
+                ansTime = (customerPos - stPos + l) % l; // 손님이 기다려야 하는 시간
+                ansTime += c->second.t;
             }
 
-            if (ansTime < 0)
-                ansTime += l;
-
-            if (waitingTime >= ansTime)
-            { // 초밥 먹기
-                //cout << t << ' ' << s.eaten << '\n';
-                s.eaten = true;
-                sushiCnt--;
-                
-                c->second.n--;
-                if (c->second.n == 0)
-                {
-                    customerCnt--;
-                    break;
-                }
-                
-            }
-
+            c->second.exitTime = max(c->second.exitTime, ansTime); // 손님이 나갈 시간
+            // 손님의 나갈 시간의 최대값이 그 손님의 퇴장 시간이 됨
+            //cout << ansTime << '\n';
+            ans.push_back({ansTime, 101}); // 초밥 사라지는 쿼리
         }
     }
 }
 void Cook(long long t, long long x, string name)
 {
-    sushiCnt++;
+    ans.push_back({t, 100});
+
     Sushi s;
     s.name = name;
     s.x = x;
     s.t = t;
-    s.eaten = false;
     sushi[name].push_back(s);
 }
 
 void Enter(long long t, long long x, string name, long long n)
 {
-    // 동일한 손님이 방문하는 경우는 없음
-    // t에 x로 가서 name손님이 앉음
-    // t에 초밥 회전이 일어난 직후에 발생
-    // 위치 x로 오는 초밥들 중 자신의 name이 적힌 초밥 n개 먹고 떠남 (시각 t에 있으면 즉시 먹음)
-    // 위치 x에 다른 사람은 없음
-    // 초밥 먹을때 시간 소요 X
-    customerCnt++;
+    ans.push_back({t, 200});
 
     Customer c;
+    c.exitTime = 0;
     c.t = t;
     c.x = x;
     c.name = name;
@@ -105,9 +80,7 @@ void Enter(long long t, long long x, string name, long long n)
 
 void Print(long long t)
 {
-    Eat(t);
-
-    cout << customerCnt << ' ' << sushiCnt << '\n';
+    ans.push_back({t, 300});
 }
 
 int main()
@@ -115,7 +88,6 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // 여기에 코드를 작성해주세요.
     cin >> l >> q;
     for (int i = 0; i < q; i++)
     {
@@ -136,6 +108,42 @@ int main()
         {
             cin >> t;
             Print(t);
+        }
+    }
+
+    Eat();
+
+    for (auto c = customer.begin(); c != customer.end(); c++)
+    {
+        // 손님 퇴장 처리
+        ans.push_back({c->second.exitTime, 201});
+    }
+
+    sort(ans.begin(), ans.end());
+
+    int sushiCnt = 0, customerCnt = 0;
+    for (auto a : ans)
+    {
+        //cout << a.second << ' ';
+        if (a.second == 100)
+        {
+            sushiCnt++;
+        }
+        else if (a.second == 101)
+        {
+            sushiCnt--;
+        }
+        else if (a.second == 200)
+        {
+            customerCnt++;
+        }
+        else if (a.second == 201)
+        {
+            customerCnt--;
+        }
+        else
+        {
+            cout << customerCnt << ' ' << sushiCnt << '\n';
         }
     }
 
